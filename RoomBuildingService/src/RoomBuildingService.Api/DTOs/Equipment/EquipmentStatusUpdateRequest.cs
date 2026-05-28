e8039d035feed6039e4b34bc65e2839dd5c5cb6b
quang2005asd <quangcokhi2005@gmail.com>
2026-05-28 08:04:11 +0700

    update

--- RoomBuildingService/src/RoomBuildingService.Api/DTOs/Equipment/EquipmentStatusUpdateRequest.cs
+++
@@ -1,6 +0,0 @@
-namespace RoomBuildingService.Api.DTOs.Equipment;
-using System.ComponentModel.DataAnnotations;
-public class EquipmentStatusUpdateRequest
-{
-    [Required] public string Status { get; set; } = null!; // ACTIVE | UNDER_MAINTENANCE | BROKEN | RETIRED
-}
